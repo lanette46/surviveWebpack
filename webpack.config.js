40b92d1f9bb20a6e0530820750353d1d077886f3
Lane C <choothohong@hotmail.com>
2019-11-09 02:14:54 +0800

    1.6 setup hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   devServer: {
     // Display only errors to reduce the amount of output.
@@ -14,10 +16,19 @@ module.exports = {
     port: process.env.PORT, // Defaults to 8080
     open: true, // Open the page in browser
     overlay: true,
+
+    // Don't refresh if hot loading fails. Good while
+    // implementing the client interface.
+    hotOnly: true,
+    // If you want to refresh on errors too, set
+    // hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Webpack demo",
     }),
+    // Enable the plugin to let webpack communicate changes
+    // to WDS. --hot sets this automatically!
+    new webpack.HotModuleReplacementPlugin(),
   ],
 };
